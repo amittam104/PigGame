e52f7f34901148febd4708eb46a81af902fc7cd7
Amit Tambulkar <amittambulkar104@gmail.com>
2023-12-02 19:05:04 +0530

    Game logic building

--- app.js
+++ app.js
@@ -18,18 +18,23 @@
 // ---------------------------------------------------------------------------------
 
 // Variables Declaration
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const current0El = document.querySelector(".current--0");
 const current1El = document.querySelector(".current--1");
 const diceEl = document.querySelector(".dice");
 const scores0El = document.querySelector(".scores--0");
 const scores1El = document.querySelector(".scores--1");
 const btnRollEl = document.querySelector(".btn--roll");
+// const activePlayer = document.querySelector(".player--active");
 let currentScore = 0;
 
 // Initial Conditions
+const score = [0, 0];
 scores0El.textContent = 0;
 scores1El.textContent = 0;
 diceEl.classList.add("hidden");
+let activePlayer = 0;
 
 // Event Listner - Roll Dice
 btnRollEl.addEventListener("click", function () {
@@ -42,5 +47,26 @@ btnRollEl.addEventListener("click", function () {
   currentScore += diceRoll;
 
   // Display that number as the current score of active player
-  current0El.textContent = currentScore;
+  // current0El.textContent = currentScore;
+  document.querySelector(`.current--${activePlayer}`).textContent =
+    currentScore;
+
+  // If dice rolls 1
+  if (diceRoll === 1) {
+    // The current score of the active player will be 0
+    currentScore = 0;
+    document.querySelector(`.current--${activePlayer}`).textContent = 0;
+
+    // The active player will change
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    currentScore += diceRoll;
+    document.querySelector(`.current--${activePlayer}`).textContent =
+      currentScore;
+    player0El.classList.toggle("player-active");
+    player1El.classList.toggle("player-active");
+  }
 });
